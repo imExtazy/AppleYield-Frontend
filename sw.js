d4b7633dbd8c2ab06bf6de0e181ddb6fb9e7d708
imExtazy <nikolaykopylov2005@gmail.com>
2025-11-11 02:40:29 +0300

    Updates

--- sw.js
+++ sw.js
@@ -5,5 +5,9 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
+// Keep a no-op fetch handler to satisfy some PWA checks, but don't intercept
+self.addEventListener('fetch', () => {
+  // no-op
+});
 
 
