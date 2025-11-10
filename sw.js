6f1437fcab8d2da057838b0d978a1312157a1c09
imExtazy <nikolaykopylov2005@gmail.com>
2025-11-11 01:21:14 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'appleyield-cache-v1';
+const CORE_ASSETS = [
+  '/AppleYield-Frontend/',
+  '/AppleYield-Frontend/index.html',
+  '/AppleYield-Frontend/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? undefined : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((cached) => cached || caches.match('/AppleYield-Frontend/index.html')))
+  );
+});
+
+
